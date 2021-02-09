5d529f66601f557e6421e14f2a3b7abfdcd22d0c
Nathan Schrader <nathanschrader@icloud.com>
2021-02-09 10:53:22 -0800

    add handleClick method to TicketControl

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -7,3 +7,5 @@ function NewTicketForm(props) {
     </React.Fragment>
   );
 }
+
+export default NewTicketForm;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -8,12 +8,29 @@ class TicketControl extends React.Component {
   
   constructor(props)  {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
   }
-
+  
+  handleClick = () => {
+    this.setState({formVisibleOnPage: true});
+  }
+  //class components always need to have render() method
+  //code inside render() method determines the content that will be rendered for user
   render()  {
+    let currentlyVisibleState = null;
+    let addTicketButton = null;
+    if (this.state.formVisibleOnPage) {         // if formVisibleOnPage is True
+      currentlyVisibleState = <NewTicketForm /> // then, show NewTicketForm
+    } else  {                                   // if formVisibleOnPage is False
+      currentlyVisibleState = <TicketList />    // then, show TicketList
+      addTicketButton = <button onClick={this.handleClick}>Add ticket</button>
+    }
     return  (
       <React.Fragment>
+        {currentlyVisibleState}
+        {addTicketButton}
       </React.Fragment>
     );
   }
